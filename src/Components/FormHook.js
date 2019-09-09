1eda4ea37c1a2de9c02c8f17d67f8769df73510c
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-09-09 18:06:49 -0400

    created a use folder for hook functions

--- src/Components/FormHook.js
+++ src/Components/FormHook.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
+import inputHook from "../hooks/useInputHook.js";
 
 const FormHook = () => {
   const [inputValue, setInputValue] = useState("");
+
+  //this one is the same as handle change in a classfull component
+  const [email, updateEmail] = inputHook("");
 
   return (
     <div>
@@ -11,6 +15,9 @@ const FormHook = () => {
         value={inputValue}
         onChange={e => setInputValue(e.target.value)}
       />
+
+      <h1>{email}</h1>
+      <input type="text" value={email} onChange={updateEmail} />
     </div>
   );
 };
